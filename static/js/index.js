d8060ebf4300204d31aa6a0d4507ee0bfd602151
vrajpatel-18 <vrajpatel.200518@gmail.com>
2023-11-11 05:00:37 -0500

    added search options on index page and api tests

--- static/js/index.js
+++ static/js/index.js
@@ -8,6 +8,19 @@ document.getElementById("search").addEventListener('keydown', function(event) {
 function search() {
     let searchValue = document.getElementById("search").value;
     if (searchValue != "") {
-        
+
     }
 }
+
+
+let searchOptions = document.querySelectorAll('.search-type');
+searchOptions.forEach(function (option) {
+    option = option.querySelector('.search-type-inner');
+    option.addEventListener('click', function (e) {
+        searchOptions.forEach(function (option) {
+            option = option.querySelector('.search-type-inner');
+            option.classList.remove('active');
+        });
+        option.classList.add('active');
+    });
+});
--- test.py
+++ test.py
@@ -1,28 +1,57 @@
 import requests
 import json
+from difflib import SequenceMatcher
 
-url = 'https://api.spotify.com/v1/search'
+
+def similar(a, b):
+    return SequenceMatcher(None, a, b).ratio()
+
+
+searchTerm = "d"
+searchLimit = '20'
+
+
+searchType = "artist"
+url = 'https://api.spotify.com/v1/search?q=' + searchTerm + '&type=' + searchType + '&limit=' + searchLimit
 headers = {
-    'Authorization': 'Bearer BQCh0jC9S36ov6rQPPRIO2cOtWyVdekyxOHSCRR7XuqDFoPQa_GjCI2C5jPzb5eEmjG8RVlucHCqXut4PbVVbazPBeQIavOXfsyNFK8T9j62Jp_gOSo'
+    'Authorization': 'Bearer BQBsL9ThtgScnGansttCP69eXffmWd7N7g1n5LA9uTztriL2JGnl7H9rbB3AI1nQHoTX4QNND62czMsgUuOHwHAy3wsNzKx9f67D9l5wXlz5TH5qPfU'
 }
+artist_response = requests.get(url, headers=headers)
 
-params = {
-    'q': 'travis',
-    'type': 'artist',
-    'market': 'US',
-    'limit': 10
+searchType = "album"
+url = 'https://api.spotify.com/v1/search?q=' + searchTerm + '&type=' + searchType + '&limit=' + searchLimit
+headers = {
+    'Authorization': 'Bearer BQBsL9ThtgScnGansttCP69eXffmWd7N7g1n5LA9uTztriL2JGnl7H9rbB3AI1nQHoTX4QNND62czMsgUuOHwHAy3wsNzKx9f67D9l5wXlz5TH5qPfU'
 }
+album_response = requests.get(url, headers=headers)
 
-response = requests.get(url, headers=headers, params=params)
 
-if response.status_code == 200:
-    data = response.json()
+artist_data = artist_response.json()
+album_data = album_response.json()
 
-    json_data = data['artists']['items'][0]
-    formatted_json = json.dumps(json_data, indent=4)
-    print(formatted_json)
-else:
-    print(f"Request failed with status code {response.status_code}")
+artists = []
+artistsPop = []
+for artist in artist_data['artists']['items']:
+    artists.append(artist['name'])
+    artistsPop.append(artist['followers']['total'])
+    
+for i in range(len(artists)):
+    # print(artist + ": " + str(similar(searchTerm, artist)))
+    print(artists[i], '{:,}'.format(artistsPop[i]))
+    
+
+# albums = []
+# for album in album_data['albums']['items']:
+#     albums.append(album['name'])
+    
+# for album in albums:
+#     print(album + ": " + str(similar(searchTerm, album)))
+
+# json_data = artist_data
+# formatted_json = json.dumps(json_data, indent=4)
+# print(formatted_json)
+
+
 
 
 # import requests
@@ -42,7 +71,7 @@ else:
 # if response.status_code == 200:
 #     token_data = response.json()
 #     access_token = token_data.get("access_token")
-#
+
 #     print(f"Access Token: {access_token}")
 # else:
 #     print(f"Token request failed with status code {response.status_code}")
